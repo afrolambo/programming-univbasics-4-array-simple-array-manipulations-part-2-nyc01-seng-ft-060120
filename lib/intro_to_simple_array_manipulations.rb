2683ee063653c6624e1e1a7e8a21b1c5f029189e
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-19 20:41:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,6 +3,7 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
+  array = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
   array[4].insert("#{element}")
   
 end
