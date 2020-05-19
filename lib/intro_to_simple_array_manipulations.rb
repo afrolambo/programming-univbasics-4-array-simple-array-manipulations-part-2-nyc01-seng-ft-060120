f1290b72c43eb970a34bff049fa918f0f4f1380f
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-19 20:43:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,9 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-
+array = []
 def using_insert(array, element)
+  array.insert[4](element)
 end
 
 def using_uniq
@@ -16,3 +17,4 @@ def using_delete
 end
 
 def using_delete_at
+end
